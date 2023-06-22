afccf9b06535e9a2f790b8d1ee2f349c2a3a9a78
SelimTekin <tekinselim.57.57@gmail.com>
2023-06-22 16:39:15 +0300

    mongoose ile veritabanına kayıt etme ve çekme

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const Post = require('./models/Post');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  const blog = { id: 1, title: "Blog title", description: "Blog description" }
+// template engine'imizin ejs olduğunu belirttik
+app.set('view engine', 'ejs');
 
-  res.send(blog);
+// MIDDLEWARES
+app.use(express.static('public')); // static dosyalarımızın public klasörü içinde olduğunu belirtik.
+app.use(express.urlencoded({ extended: true })); // url'deki datayı okumamızı sağlar.
+app.use(express.json()); // url'deki datayı json formatına döndürür.
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
